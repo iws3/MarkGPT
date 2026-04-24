eaf87c35bd3f64b42c2adfa45d80e29790fcfdaa
Gody-web <blessfannyuy0@yahoo.com>
2026-04-24 17:50:38 +0100

    solutions to strings exercises
    co-authored-by: iws3 <iwtechnical7@gmail.com>

---
+++ contributors/Gody-web/strings exercises/ex03.py
@@ -0,0 +1,4 @@
+# exercise to replace hello with the next letter
+# solution 
+message= "Hello";
+print(chr(ord(message[0])+1),chr(ord(message[1])+1),chr(ord(message[2])+1),chr(ord(message[3])+1),chr(ord(message[4])+1))
