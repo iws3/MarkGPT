6f911028bd4887a5297439f2a8765ca9e74f058a
iws-seed <iwstechnical7@gmail.com>
2026-06-29 12:43:11 +0100

    explored first part of tensors maths : basic math operations

--- contributors/gita/module-04/pytorch/tensors.py
+++ contributors/gita/module-04/pytorch/tensors.py
@@ -1,7 +1,7 @@
 import sys
 import torch
 
-print(f"Python version: {sys.version}")
+print(f"Python version: {sys.version}") 
 print(f"Pytorch version: {torch.__version__}")
 
 print(f"CUDA avalaibility: {torch.cuda.is_available()}")
