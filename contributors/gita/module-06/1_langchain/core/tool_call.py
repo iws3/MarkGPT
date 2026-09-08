ac71a4eef5096de5df74eacf0b6c10b5afdc96ff
Technical Team <iwstechnical7@gmail.com>
2026-09-08 10:03:29 +0100

    explored the tool calling mechanism and how functions are actualy called in the llms

--- contributors/gita/module-06/1_langchain/core/tool_call.py
+++ contributors/gita/module-06/1_langchain/core/tool_call.py
@@ -1,7 +1,7 @@
 from utils.tools import get_module_deadline, count_students_in_module
 
 from .models import create_model
-from langchain_core.messages import HumanMessage, AIMessage, ToolMessage
+from langchain_core.messages import HumanMessage, AIMessage, ToolMessage, SystemMessage
 
 model=create_model()
 tools=[get_module_deadline, count_students_in_module]
@@ -11,9 +11,13 @@ llm=model.bind_tools(tools)
 
 # ____________________
 
-tools_by_name=[t.name for t in tools]
+tools_by_name={t.name: t for t in tools}
+# see=tools_by_name["count_students_in_module"]
 
-# print(tools_by_name)
+# StructuredTool(name='get_module_deadline', description='Look up the submission deadline for a name bootcamp module.', args_schema=<class 'langchain_core.utils.pydantic.get_module_deadline'>, func=<function get_module_deadline at 0x000001E48ACAE200>), 'count_students_in_module': is the value of t.name
+
+
+print(tools_by_name)
 messages=[HumanMessage("How many studnts are in the CNN module and when is it due?")]
 
 ai_response=llm.invoke(messages)
@@ -21,5 +25,15 @@ print(f"The Ai response is: {ai_response}")
 messages.append(ai_response)
 print(f"The messages array is: {messages}")
 
+
 for tool in ai_response.tool_calls:
-    print("hello there")
+    print(f"TOOL IS: {tool}")
+    tool_fn=tools_by_name[tool["name"]]
+    result=tool_fn.invoke(tool["args"])
+    print(f"Result is: {result}")
+    messages.append(ToolMessage(content=result, tool_call_id=tool["id"]))
+    
+print(f"The messages list is : {messages}")
+messages.append(SystemMessage(content="Please make sure you extend the result with moe content of up to 70 words .. be explicit"))
+final=model.invoke(messages)
+print(final.content)
