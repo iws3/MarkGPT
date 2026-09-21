77d7162db96fca5876204305e201d8916dafef7f
gitaxin <iwstechnical7@gmail.com>
2026-09-21 09:18:52 +0100

    started adding content for RAGS

---
+++ contributors/gita/module-06/1_langchain/rag/main.py
@@ -0,0 +1,12 @@
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
+pdf_loader = PyPDFLoader("doc.pdf")
+pdf_docs = pdf_loader.load()
+# one Document per page, with page number in metadata
+text_loader = TextLoader("module_notes.txt")
+text_docs = text_loader.load()
+print(f"Loaded {len(pdf_docs)} pages from the PDF")
+print(pdf_docs[0].metadata)
+print(f"Printitng the pdf docs: {pdf_docs}")
+# e.g. {'source': '...', 'page': 0}
+# pip install langchain_community
+# pip install pypdf
