c33069d9043ae13d22a24de66e013847c34a400e
gitaxin <iwstechnical7@gmail.com>
2026-09-21 16:06:29 +0100

    explored real chunking size from first principles

---
+++ contributors/gita/module-06/1_langchain/rag/chunk_size_comparison.py
@@ -0,0 +1,19 @@
+# chunk_size_comparison.py
+
+from langchain_community.document_loaders import TextLoader, PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_huggingface import HuggingFaceEmbeddings
+
+docs=TextLoader("docs/my_document.txt").load()
+
+
+for chunk_size in [300, 800, 1500]:
+    # calculate the overlap size
+    overlap=int(chunk_size * 0.15)
+    splitter=RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=overlap)
+    chunks=splitter.split_documents(docs)
+    # avg_length=sum(len(c.page_content)  for c in chunks)
+    len_content=[len(c.page_content) for c in chunks]
+    print(f"Lenght for {chunk_size} is : {len_content}")
+    
